e81f595ab464e8121619d9437c38e53b37832ea5
Ankur Sharma <>
2019-04-09 16:30:06 +0530

    initial project setup

---
+++ SwiftLib/SwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLib.h
+//  SwiftLib
+//
+//  Created by Ankur Sharma on 09/04/19.
+//  Copyright © 2019 com.ankur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLib.
+FOUNDATION_EXPORT double SwiftLibVersionNumber;
+
+//! Project version string for SwiftLib.
+FOUNDATION_EXPORT const unsigned char SwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib/PublicHeader.h>
+
+
