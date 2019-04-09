d43134f82c326c557018e5ea14a0182ad9317399
Ankur Sharma <>
2019-04-09 16:50:28 +0530

    add travis

---
+++ SwiftLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftLib
+//
+//  Created by Ankur Sharma on 09/04/19.
+//  Copyright © 2019 com.ankur. All rights reserved.
+//
+
+import Foundation
+
+class SwiftyLib{
+  let name = "SwiftyLib"
+  
+  func add (a:Int, b:Int)-> Int{
+    return a + b
+  }
+
+  func sub(a: Int,b: Int)->Int {
+    return a - b
+  }
+  
+  
+}
--- SwiftLibTests/SwiftLibTests.swift
+++ SwiftLibTests/SwiftLibTests.swift
@@ -11,8 +11,11 @@ import XCTest
 
 class SwiftLibTests: XCTestCase {
 
+  var sut: SwiftyLib!
+  
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+      sut = SwiftyLib()
     }
 
     override func tearDown() {
@@ -22,8 +25,18 @@ class SwiftLibTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
     }
 
+  func testAdd(){
+      XCTAssertEqual(sut.add(a: 2, b: 2), 4)
+  }
+  
+  func testSub(){
+      XCTAssertEqual(sut.sub(a: 2, b: 2), 0)
+  }
+  
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
